440164fad15f027d8bd9da94227c3f149e0750ed
João Caetano <118355857+joaovscaetano@users.noreply.github.com>
2024-09-24 08:22:42 -0400

    Update main.ts

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+// LIVE SERVER é do FRONT-END
+// QUEM É O LIVE SERVER DO BACK-END?
+
+// 1 - Para construir um servidor back-end e responder
+// Vamos utilizar o EXPRESS
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql2/promise'
+//Criar um objeto do tipo express.
+const app = express()
+//incluir pra ele receber json
+app.use(express.json())  //Middleware
+//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
+app.use(cors())
+//ROTAS
+app.get("/produtos",async(req,res)=>{
+
+    //O que eu tenho que fazer aqui dentro?
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    try {
+        const conexao = await mysql.createConnection({
+            host:"localhost",
+            user:"root",
+            password:"",
+            database:"banco1022b",
+            port:3306
+        })
+    }catch(e){
+        res.status(504).send("Erro do servidor")
+    }
+    //PASSO 3: QUERY  -> SELECT * FROM produtos
+    //PASSO 4: Colocar os dados do banco no response
+    res.send({mensagem:"Eu sou a rota de produtos"})
+})
+//INICIAR O SERVIDOR
+app.listen(8000,()=>{
+    console.log("SERVIDOR INICIADO NA PORTA 8000")
+})
