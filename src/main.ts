3752c6bd500b55cad907de1b86d30965a5d06b39
João Caetano <118355857+joaovscaetano@users.noreply.github.com>
2024-10-01 07:14:16 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,27 +13,31 @@ app.use(express.json())  //Middleware
 //incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
-app.get("/produtos", async (req, res) => {
+app.get("/produtos",async(req,res)=>{
 
     //O que eu tenho que fazer aqui dentro?
     //OK -> PASSO 1: Criar o banco de dados
     //PASSO 2: Usar a lib mysql2 para conectar com o banco
-    try {
+    try{
         const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022b",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-    } catch (e) {
-        res.status(504).send("Erro do servidor")
+        //PASSO 3: QUERY  -> SELECT * FROM produtos
+        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
+        //PASSO 4: Colocar os dados do banco no response
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
     }
-    //PASSO 3: QUERY  -> SELECT * FROM produtos
-    //PASSO 4: Colocar os dados do banco no response
-    res.send({ mensagem: "Eu sou a rota de produtos" })
+    
+    
 })
+
 //INICIAR O SERVIDOR
-app.listen(8000, () => {
+app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })
